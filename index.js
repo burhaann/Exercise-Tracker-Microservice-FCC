da30de6ff45e31edc8357db21ed00b1fead6444c
Burhaann <burhaann321@gmail.com>
2023-12-01 15:45:24 +0530

    started testing

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose
   .connect(process.env.MONGO_URI)

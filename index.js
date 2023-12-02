9be469a7a8c26efb55b5536ff37fafbc084eb726
Burhaann <burhaann321@gmail.com>
2023-12-02 12:06:41 +0530

    test 6 onwards

--- index.js
+++ index.js
@@ -119,12 +119,22 @@ app.post("/api/users/:_id/exercises", async function (req, res) {
   }
 });
 
-app.get("/api/users/:_id/logs", function (req, res) {
+app.get("/api/users/:_id/logs", async function (req, res) {
   const _id = req.params._id;
+  let log;
+  const user = await User.findById(_id);
   Exercise.find({ userid: _id })
     .select("-__v")
     .then((users) => {
-      res.send(users);
+      log = {
+        username: user.username,
+        count: users.length(),
+        _id: user._id,
+        log: users,
+      };
+
+      res.send(log);
+      console.log(users);
       console.log(users);
     });
 });
